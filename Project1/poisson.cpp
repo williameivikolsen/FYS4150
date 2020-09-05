25c254f684907eda3a09ca4bdae7b0bffb752361
william <williaeo@student.matnat.uio.no>
2020-09-05 14:08:15 +0200

    Fikset regnefeil og la inn kommentarer

--- Project1/poisson.cpp
+++ Project1/poisson.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <cmath>
-#include <fstream>
-#include <iomanip>
-#include "time.h"
-#include <chrono>
-#include <ctime>
+#include <fstream>      // Write to file
+#include <iomanip>      // Text formatting
+#include "time.h"       // Timer
+#include <chrono>       // Current time
+#include <ctime>        // Print current time
 // use namespace for output and input
 using namespace std;
 
-//outputfile as global variable
-ofstream ofile;
-ofstream logfile;
+ofstream ofile;     // File to store results
+ofstream logfile;   // File to log time spent
 
 double f(double x) {
     return 100*exp(-10*x);
 }
 
 void general(int n, double *d, double *a, double *b, double *c) {
-    double *dn, *bn, *v;     // Updated values after forward substitution and unkown
-    dn = new double[n]; bn = new double[n]; v = new double[n+2];
-    // Lager outfile:
+    // Function that solves equation for the general
+    // tridiagonal matrix case
+
+    double *dn = new double[n];     // Updated diagonal elements of matrix
+    double *bn = new double[n];     // Updated right side of equation h²f(x)
+    double *v = new double[n+2];    // Solution to equation
+    // Make outfile:
     string outfilename = "general_";
     outfilename.append(to_string(n));
     outfilename.append(".txt");
@@ -37,13 +40,14 @@ void general(int n, double *d, double *a, double *b, double *c) {
     v[0] = 0;
     v[n] = bn[n-1]/dn[n-1];
     for(int i = n-1; i > 0; i--) {
-        v[i] = (bn[i]-(c[i]*v[i+1]))/dn[i];
+        v[i] = (bn[i-1] - c[i-1]*v[i+1])/dn[i-1];
     }
+    // Write to file:
     for(int i = 0; i < n+2; i++) {
         ofile << setw(15) << setprecision(8) << v[i] << endl;
     }
     
-
+    // Clear memory
     delete [] dn; delete [] bn; delete [] v;
 }
 
@@ -53,23 +57,25 @@ void special(int n, double *d, double *e, double *b) {
 }
 
 int main(int argc, char *argv[]){
-    // Read output file name and number of grid points
-    int n;
-    string function;
+    // Read number of grid points and function to be used
     if( argc != 3 ){
           cout << "Bad Usage: " << argv[0] <<
               " needs number of grid points and function when executed" << endl;
           exit(1);
     }
         else{
-        n = atoi(argv[1]);
-        function = argv[2];
+        int n = atoi(argv[1]);
+        string function = argv[2];
     }
-
-    double h = 1.0/(n+2.0);     // Step size
-    double hh = h*h;
-    double *d, *a, *b, *c, *x;
-    d = new double[n]; a = new double[n-1]; b = new double[n]; c = new double[n-1]; x = new double[n+2];  // Se algoritmetekst
+    // Prepare for calculation
+    double h = 1.0/(n+2.0);         // Step size
+    double hh = h*h;                // Step size squared
+    double *d = new double[n];      // Diagonal elements of matrix 
+    double *a = new double[n-1];    // Lower diagonal elements
+    double *b = new double[n];      // Right side of equation, h²f(x)
+    double *c = new double[n-1];    // Upper diagonal elements
+    double *x = new double[n+2];    // x coordinates
+    // Fill in values of arrays
     for(int i = 0; i < n+2; i++) {
         x[i] = double(i*h);
     }
@@ -79,9 +85,13 @@ int main(int argc, char *argv[]){
         c[i] = -1;
         b[i] = hh*f(x[i+1]);
     }
+    // Last elements of d and b not included in loop:
     d[n-1] = 2;
     b[n-1] = hh*f(x[n]);
+
+    // Open log file
     logfile.open("log.txt", std::ios_base::app);
+    // Check function and execute
     if(function == "general") {
         clock_t start, finish;
         start = clock();
@@ -91,7 +101,7 @@ int main(int argc, char *argv[]){
         auto timenow = chrono::system_clock::to_time_t(chrono::system_clock::now());
         logfile << function << "    " << n << "    " << timeused << "    " << ctime(&timenow) << endl;
     }
-    else if(function == "special") {
+        else if(function == "special") {
         double *e;
         clock_t start, finish;
         start = clock();
@@ -101,7 +111,7 @@ int main(int argc, char *argv[]){
         auto timenow = chrono::system_clock::to_time_t(chrono::system_clock::now());
         logfile << function << "    " << n << "    " << timeused << "    " << ctime(&timenow) << endl;
     }
-    else {
+        else {
         cout << "The function you specified does not exist." << endl;
         exit(1);
     }
