cdaf243b6abd5d855bbf36eff0f96e079f7e5d59
william <williaeo@student.matnat.uio.no>
2020-09-05 17:16:04 +0200

    Fikset spesiell algoritme, kjørte for oppgitte verdier av n

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -1,16 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-infile = open("general_101.txt")
-n = 101
-vlist = []
-for line in infile:
-    vlist.append(float(line))
-v = np.array(vlist)
+infile_sp = open("special_1000.txt")
+infile_ge = open("general_1000.txt")
+n = 1000
+vlist_sp = []
+vlist_ge = []
+for line in infile_sp:
+    vlist_sp.append(float(line))
+for line in infile_ge:
+    vlist_ge.append(float(line))
+v_sp = np.array(vlist_sp)
+v_ge = np.array(vlist_ge)
 x = np.linspace(0, 1, n+2)
-u = 1- (1-np.exp(-10))*x - np.exp(-10*x)
-plt.plot(x, u, label='Analytisk')
-plt.plot(x, v, label='Numerisk')
+x_nice = np.linspace(0, 1, 10001)
+u = 1- (1-np.exp(-10))*x_nice - np.exp(-10*x_nice)
+plt.plot(x_nice, u, label='Analytisk')
+plt.plot(x, v_sp, label='Numerisk, spesiell')
+plt.plot(x, v_ge, label='Numerisk, generell', ls='--')
 plt.grid(ls='--')
 plt.xlabel('x')
 plt.ylabel('u(x)')
--- Project1/poisson.cpp
+++ Project1/poisson.cpp
@@ -31,13 +31,18 @@ void general(int n, double *d, double *a, double *b, double *c) {
     // Forward substitution:
     dn[0] = d[0];
     bn[0] = b[0];
+
     for(int i = 1; i < n; i++) {
         dn[i] = d[i] - (a[i-1]*c[i-1])/dn[i-1];
         bn[i] = b[i] - (a[i-1]*bn[i-1])/dn[i-1];
     }
+
     // Backward substitution:
     v[n+1] = 0;
     v[0] = 0;
+
+  
+
     v[n] = bn[n-1]/dn[n-1];
     for(int i = n-1; i > 0; i--) {
         v[i] = (bn[i-1] - c[i-1]*v[i+1])/dn[i-1];
@@ -51,21 +56,58 @@ void general(int n, double *d, double *a, double *b, double *c) {
     delete [] dn; delete [] bn; delete [] v;
 }
 
-void special(int n, double *d, double *e, double *b) {
-    double *dn;
-    double *bn;
+void special(int n, double *b) {
+    // Function that solves equation for the general
+    // tridiagonal matrix case
+
+    double *dn = new double[n];    // Updated diagonal elements of matrix
+    double *bn = new double[n];    // Updated right side of equation h²f(x)
+    double *v = new double[n + 2]; // Solution to equation
+    // Make outfile:
+    string outfilename = "special_";
+    outfilename.append(to_string(n));
+    outfilename.append(".txt");
+    ofile.open(outfilename);
+
+    // We have analytical expression for dn in special case:
+    for (int i = 1; i < n+1; i++) {
+        dn[i-1] = (1.0+i)/i;
+    }
+    // Forward substitution:
+    bn[0] = b[0];
+    for (int i = 1; i < n; i++) {
+        bn[i] = b[i] + bn[i-1]/dn[i-1];
+    }
+    // Backward substitution:
+    v[n+1] = 0;
+    v[0] = 0;
+    v[n] = bn[n-1]/dn[n-1];
+    for (int i = n-1; i > 0; i--) {
+        v[i] = (bn[i-1] + v[i+1])/dn[i-1];
+    }
+    // Write to file:   
+    for (int i = 0; i < n + 2; i++)
+    {
+        ofile << setw(15) << setprecision(8) << v[i] << endl;
+    }
+
+    // Clear memory
+    delete[] dn;
+    delete[] bn;
+    delete[] v;
 }
 
 int main(int argc, char *argv[]){
     // Read number of grid points and function to be used
+    int n; string function;
     if( argc != 3 ){
           cout << "Bad Usage: " << argv[0] <<
               " needs number of grid points and function when executed" << endl;
           exit(1);
     }
         else{
-        int n = atoi(argv[1]);
-        string function = argv[2];
+        n = atoi(argv[1]);
+        function = argv[2];
     }
     // Prepare for calculation
     double h = 1.0/(n+2.0);         // Step size
@@ -80,9 +122,9 @@ int main(int argc, char *argv[]){
         x[i] = double(i*h);
     }
     for(int i = 0; i < n-1; i++) {
-        d[i] = 2;
-        a[i] = -1;
-        c[i] = -1;
+        d[i] = 2.0;
+        a[i] = -1.0;
+        c[i] = -1.0;
         b[i] = hh*f(x[i+1]);
     }
     // Last elements of d and b not included in loop:
@@ -102,10 +144,9 @@ int main(int argc, char *argv[]){
         logfile << function << "    " << n << "    " << timeused << "    " << ctime(&timenow) << endl;
     }
         else if(function == "special") {
-        double *e;
         clock_t start, finish;
         start = clock();
-        special(n, d, e, b);
+        special(n, b);
         finish = clock();
         double timeused = (double)(finish - start) / (CLOCKS_PER_SEC);
         auto timenow = chrono::system_clock::to_time_t(chrono::system_clock::now());
