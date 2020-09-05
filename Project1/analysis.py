64ca0b61ca308dd1357496640bfc491baeb9919f
william <williaeo@student.matnat.uio.no>
2020-09-05 12:22:15 +0200

    La til n=1001

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-infile = open("general_101.txt")
-n = 101
+infile = open("general_1001.txt")
+n = 1001
 vlist = []
 for line in infile:
     vlist.append(float(line))
