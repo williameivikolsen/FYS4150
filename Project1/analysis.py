1104377fc10f35e7d0b3b4829c266a6027eb219e
william <williaeo@student.matnat.uio.no>
2020-09-06 12:43:33 +0200

    Endringer i analysis.py

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -1,35 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-infile_sp = open("special_1000.txt")
-infile_ge = open("general_1000.txt")
-n = 1000
-vlist_sp = []
-vlist_ge = []
-for line in infile_sp:
-    vlist_sp.append(float(line))
-for line in infile_ge:
-    vlist_ge.append(float(line))
-v_sp = np.array(vlist_sp)
-v_ge = np.array(vlist_ge)
-x = np.linspace(0, 1, n+2)
-u = 1- (1-np.exp(-10))*x - np.exp(-10*x)
-plt.plot(x, u, label='Analytisk')
-plt.plot(x, v_sp, label='Numerisk, spesiell')
-plt.plot(x, v_ge, label='Numerisk, generell', ls='--')
-plt.grid(ls='--')
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.legend()
-plt.show()
+n = [10, 100, 1000, 10000, 100000, 1000000]
+maxerr_sp = []
+maxerr_ge = []
+for i in n:
+    i_st = str(i)
+    infile_sp = open("special_%s.txt" % i_st)
+    infile_ge = open("general_%s.txt" % i_st)
+    vlist_sp = []
+    vlist_ge = []
+    for line in infile_sp:
+        vlist_sp.append(float(line))
+    for line in infile_ge:
+        vlist_ge.append(float(line))
+    v_sp = np.array(vlist_sp)
+    v_ge = np.array(vlist_ge)
+    x = np.linspace(0, 1, i+2)
+    u = 1- (1-np.exp(-10))*x - np.exp(-10*x)
+    plt.plot(x, u, label='Analytical')
+    plt.plot(x, v_sp, label='Numerical, special')
+    plt.plot(x, v_ge, label='Numerical, general', ls='--')
+    plt.title('Comparison for n = %s' % i_st)
+    plt.grid(ls='--')
+    plt.xlabel('x')
+    plt.ylabel('u(x)')
+    plt.legend()
+    plt.savefig('plot_n%s' % i_st)
+    plt.close()
+    relerr_sp = np.abs((v_sp - u)/u)[1:-1]
+    relerr_ge = np.abs((v_ge - u)/u)[1:-1]
+    maxerr_sp.append(np.max(relerr_sp))
+    maxerr_ge.append(np.max(relerr_ge))
 
-relerr_sp = np.log10(np.abs((v_sp - u)/u))
-relerr_ge = np.log10(np.abs((v_ge - u)/u))
-plt.plot(x, relerr_sp, label='Special algorithm')
-plt.plot(x, relerr_ge, label='General algorithm')
-plt.title('Relative error')
+plt.loglog(n, maxerr_sp, 'o', label='Special algorithm')
+plt.loglog(n, maxerr_ge, 'o', label='General algorithm')
+plt.title('Maximum relative error')
 plt.grid(ls='--')
-plt.xlabel('x')
-plt.ylabel(r'$\epsilon (x)$')
+plt.xlabel('n')
+plt.ylabel(r'$\epsilon (n)$')
 plt.legend()
 plt.show()
