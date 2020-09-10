1d5ee0d8280a61bbdae9c31359e90b19ee9221f3
pederlh <36080334+pederlh@users.noreply.github.com>
2020-09-10 20:03:02 +0200

    La til CPU-tid-plott i analysis.py

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -96,3 +96,46 @@ plt.legend()
 plt.savefig('maxrel')
 plt.show()
 plt.close()
+
+# Start CPU time plot -> no average, one value
+
+time_sp = np.array([1.00e-06,1.00e-06,1.30e-05,1.40e-04,1.52e-03,1.45e-02,1.47e-01])
+time_ge = np.array([1.00e-06,2.00e-06,1.60e-05,2.04e-04,1.96e-03,2.16e-02,1.95e-01])
+time_LU = np.array([8.70e-05, 1.71e-04, 9.16e-02, 9.50e+00])
+
+plt.loglog(n_arr, time_sp, 'o', label='Special algorithm')
+plt.loglog(n_arr, time_ge, 'x', label='General algorithm')
+plt.loglog(n_arr[:4], time_LU, '*', label='LU-decomposition')
+
+plt.grid(ls='--')
+plt.xlabel(r'$h$')
+plt.ylabel(r'CPU time (s)')
+plt.legend()
+plt.savefig('time')
+plt.show()
+plt.close()
+
+
+
+"""
+Time values taken from following data from logfile
+
+special      1.0e+01  1.00e-06     Thu Sep 10 19:41:50 2020
+special      1.0e+02  1.00e-06     Thu Sep 10 19:41:50 2020
+special      1.0e+03  1.30e-05     Thu Sep 10 19:41:50 2020
+special      1.0e+04  1.40e-04     Thu Sep 10 19:41:50 2020
+special      1.0e+05  1.52e-03     Thu Sep 10 19:41:50 2020
+special      1.0e+06  1.45e-02     Thu Sep 10 19:41:50 2020
+special      1.0e+07  1.47e-01     Thu Sep 10 19:41:54 2020
+general      1.0e+01  1.00e-06     Thu Sep 10 19:42:31 2020
+general      1.0e+02  2.00e-06     Thu Sep 10 19:42:31 2020
+general      1.0e+03  1.60e-05     Thu Sep 10 19:42:31 2020
+general      1.0e+04  2.04e-04     Thu Sep 10 19:42:31 2020
+general      1.0e+05  1.96e-03     Thu Sep 10 19:42:31 2020
+general      1.0e+06  2.16e-02     Thu Sep 10 19:42:31 2020
+general      1.0e+07  1.95e-01     Thu Sep 10 19:42:36 2020
+LU           1.0e+01  8.70e-05     Thu Sep 10 19:43:11 2020
+LU           1.0e+02  1.71e-04     Thu Sep 10 19:43:12 2020
+LU           1.0e+03  9.16e-02     Thu Sep 10 19:43:12 2020
+LU           1.0e+04  9.50e+00     Thu Sep 10 19:43:27 2020
+"""
