14850f4acccc6742ca92be3cdf71a31ed839f65f
william <williaeo@student.matnat.uio.no>
2020-09-29 14:37:12 +0200

    Fikset liten feil med generering av matriser

--- Project2/beam.cpp
+++ Project2/beam.cpp
@@ -5,11 +5,11 @@ void Beam::Init(int n, double epsilon, int maxit) {
     Initialize(n, epsilon, maxit);
     // Genererer matrise:
     for (int i = 0; i < m_n - 1; i++) {
-        m_A(i, i) = m_a;
-        m_A(i + 1, i) = m_d;
-        m_A(i, i + 1) = m_d;
+        m_A(i, i) = m_d;
+        m_A(i + 1, i) = m_a;
+        m_A(i, i + 1) = m_a;
     }
-    m_A(m_n - 1, m_n - 1) = m_a;
+    m_A(m_n - 1, m_n - 1) = m_d;
 }
 
 void Beam::Solve() {
