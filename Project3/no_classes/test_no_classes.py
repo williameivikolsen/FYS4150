b8a27ac84f2ea022cb3e8ef503f8031a7b965515
pederlh <36080334+pederlh@users.noreply.github.com>
2020-10-12 12:50:00 +0200

    Start making tests in python

---
+++ Project3/no_classes/test_no_classes.py
@@ -0,0 +1,76 @@
+'''
+Test program that does the following tests:
+    1) Check that solutions match with circular orbit
+    2) Check that potential + kinetic energy stays constant
+    3) Check that angular momentum is conserved
+'''
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# N = int(input("Which value of step size N to test for? "))
+# assert open("Euler_%i.txt" % N), "Output files not found for this value of N. Try again after computing results."
+N=366
+
+
+# Start by reading data from files
+infile_euler = open("Euler_%i.txt" % N, "r")   
+infile_verlet = open("Verlet_%i.txt" % N, "r")
+
+infile_euler.readline()                                 # Skip one line
+params = infile_euler.readline().split()                # Parameters located in line 2
+
+t0 = float(params[1])
+tn = float(params[2])
+h = float(params[4])
+
+infile_euler.seek(0)                                    # Reset file handle to start
+
+data_euler = np.loadtxt(infile_euler, skiprows=4)       # Actual data arrays
+data_verlet = np.loadtxt(infile_verlet, skiprows=4)     # Format rx-ry-vx-vy
+
+infile_euler.close()
+infile_verlet.close()
+
+def test_circular(data_set, metohd_name, eps=1e-2):     # Takes in either the data_euler or data_verlet
+    rx, ry = data_set[:,0], data_set[:,1]
+    theta0 = np.arctan2(ry[0], rx[0])                   # Initial angle
+    delta_theta = 2*tn*np.pi/(N-1)                      # Change of angle assuming circular orbit
+
+    rx_test = np.zeros(N)                               # Make arrays for circular orbit
+    ry_test = np.zeros(N)
+    for i in range(N):
+        rx_test[i] = np.cos(theta0 + i*delta_theta)
+        ry_test[i] = np.sin(theta0 + i*delta_theta)
+
+    # first_problem_idx = 0                                                       # First index that exceeds tolerance
+    # num_problems = 0                                                            # Number of indexes with problems
+    # deviance = np.sqrt((rx[0]-rx_test[0])**2 + (ry[0]-ry_test[0])**2);          # Initial deviance
+    # max_deviance = 0 
+    #                                                            #Store max deviance
+    # for i in range(N-1):
+    #     if deviance > eps and num_problems == 0:
+    #         first_problem_idx = i
+    #         num_problems += 1
+    #     elif(deviance > eps):
+    #         num_problems += 1
+
+    #     new_deviance = np.sqrt((rx[i+1]-rx_test[i+1])**2 + (ry[i+1]-ry_test[i+1])**2)
+    #     if new_deviance > max_deviance:
+    #         max_deviance = new_deviance
+
+    #     deviance = new_deviance
+    
+
+    # Print out results
+    print("Testing for method {}, tolerance set to {}...".format(metohd_name, eps))
+    if num_problems == 0:
+        print("The test was passed successfully!")
+        print("Max recorded deviance was {:.7f}".format(max_deviance))
+
+    else:
+        print("The test failed for {}/{} time steps".format(num_problems, N))
+        print("The problems started at index {}".format(first_problem_idx))
+        print("Max recorded deviance was {:.7f}".format(max_deviance))
+
+test_circular(data_verlet, "Verlet")
