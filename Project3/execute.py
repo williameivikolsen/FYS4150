b39ebc25997b08e3043b1dcd676f716b4aa9a531
Benedicte Nyheim <benedibn@student.matnat.uio.no>
2020-10-13 19:53:47 +0200

    Kode som funker for å lese inn initial-verdiene. Kommentert ut alle kall på SolarSystem frem til klassen funker

--- Project3/execute.py
+++ Project3/execute.py
@@ -6,17 +6,20 @@ import sys
 # ------------- Compilation -------------
 all_cpp_codes = "./*.cpp"
 os.system("echo compiling...")
-os.system("c++ -o main.exe" + " " + all_cpp_codes)
+os.system("c++ -o main.exe" + " " + "main.cpp") #all_cpp_codes)
 # ---------------------------------------
 
 # -------------- Execution --------------
 N = input("Select number of integration points: ")
 tn = input("Select simulation time (years): ")
-Nplanets = input("Select number of planets: ")
-problem = input("Choose system:")
+Nobjects = input("Select number of planets: ")
+problem = input("Choose system, sun_earth/sun_earth_jupiter/full_system/sun_mercery: ")
+
+initial_values = "./datasets/initial_conditions/initial_conditions_" + problem + ".txt"
+masses = "./datasets/masses/masses_" + problem + ".txt"
 
 os.system("echo executing...")
-os.system("./main.exe" + " " + N + " " + tn + " " + Nplanets)                     # Execute code
+os.system("./main.exe" + " " + N + " " + tn + " " + Nobjects + " " + initial_values + " " + masses)    # Execute code
 # -----------------------------------------
 
 # ------ File handling and plotting -------
@@ -24,11 +27,14 @@ os.system("./main.exe" + " " + N + " " + tn + " " + Nplanets)                   
 path = "./results"
 if not os.path.exists(path):
     os.makedirs(path) #Creates the directory
+
+"""
 euler_file = "Euler_" + N + ".txt"
 verlet_file = "Verlet_" + N + ".txt"
 os.system("mv" + " " + euler_file + " " + path)         # Move Euler data to results directory.
 os.system("mv" + " " + verlet_file + " " + path)         # Move Euler data to results directory.
 os.system("python3 plot.py " + N)
+"""
 # Open plot:
 # os.chdir(path)
 # os.system("code " + plot + "_" + N + ".png")
--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,41 +1,46 @@
-#include "SolarSystem.hpp"
+//#include "SolarSystem.hpp"
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
-
 
 int main(int argc, char *argv[]) {
     int N = atoi(argv[1]);                              // Number of inegration points
     double tn = atof(argv[2]);                          // Time to be simulated
     int Nobjects = atoi(argv[3]);                       // Number of planets
-    string objects = atoi(argv[4]);
+    char* initial_conditions = argv[4];          // File containing initial conditions for chosen system
+    char* masses = argv[5];                       // File containing masses for chosen system
 
     double *x, *y, *z, *vx, *vy, *vz;                   //To store initial conditions for each particle.
     double *mass;                                       //Store mass of particles.
-    x = new double[Nplanets];
-    y = new double[Nplanets];
-    z = new double[Nplanets];
-    vx = new double[Nplanets];
-    vy = new double[Nplanets];
-    vz = new double[Nplanets];
-    mass = new double[Nplanets];
+    x = new double[N];
+    y = new double[Nobjects];
+    z = new double[Nobjects];
+    vx = new double[Nobjects];
+    vy = new double[Nobjects];
+    vz = new double[Nobjects];
+    mass = new double[Nobjects];
 
-    char* filename_pos_vel = "positions_and_vel.txt";   //Each line of file gives initial condition for a particle on the form: x y z vx vy vz
-    char* filename_mass = "masses.txt";                 //Each line of the file contains a mass for a given particle.
+    //char* filename_pos_vel = initial_conditions;   //Each line of file gives initial condition for a particle on the form: x y z vx vy vz
+    //char* filename_mass = masses;                 //Each line of the file contains a mass for a given particle.
 
     //Open files
-    FILE *fp_init = fopen(filename_pos_vel, "r"); //Open file to read, specified by "r".
-    FILE *fp_mass = fopen(filename_mass, "r");    //Open file to read.
+    FILE *fp_init = fopen(initial_conditions, "r"); //Open file to read, specified by "r".
+    FILE *fp_mass = fopen(masses, "r");    //Open file to read.
 
     //Loop over each particle and extract its mass and initial conditions:
-    for (int i = 0; i < Nplanets; i++){
+    for (int i = 0; i < Nobjects; i++){
         fscanf(fp_init, "%lf %lf %lf %lf %lf %lf", &x[i], &y[i], &z[i], &vx[i], &vy[i], &vz[i]); // One %lf (lf=long float or double) for each floating point number on each line of the file.
         fscanf(fp_mass, "%lf", &mass[i]); //Extract mass for particle i.
     }
 
+
     fclose(fp_init); //Close file with initial conditions
     fclose(fp_mass); //Close file with masses.
 
+    cout << x[1] << endl;
+
+/*
     // Løser foreløpig kun for jorda (element 0)
     SolarSystem my_solver(F);
     my_solver.initialize(tn, N, x[0], y[0], z[0], vx[0], vy[0], vz[0]);
@@ -43,6 +48,6 @@ int main(int argc, char *argv[]) {
     my_solver.write_to_file("Euler");
     my_solver.solve_velvet();
     my_solver.write_to_file("Verlet");
-
+*/
     return 0;
 }
