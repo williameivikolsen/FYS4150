f16515e146ca5ae65b0125a34c44425eb97e0110
williameivikolsen <william@LAPTOP-BI6LPL6C.localdomain>
2020-11-19 12:37:28 +0100

    Fikset feil i metropolis-algoritme

--- Project4/IsingModel.cpp
+++ Project4/IsingModel.cpp
@@ -10,33 +10,38 @@ void IsingModel::Initialize(int L, double temp){
     m_spin = new int[m_N];
 
     // Initialiserer systemet til å starte med alle spinn opp
-    for (int i = 0; i < m_L; i++) {
-        for (int j = 0; j < m_L; j++) {
-            m_spin[i*m_L + j] = 1;
-        }
-    }
-    m_M = m_N;
+    // for (int i = 0; i < m_L; i++) {
+    //     for (int j = 0; j < m_L; j++) {
+    //         m_spin[i*m_L + j] = 1;
+    //     }
+    // }
+    // m_M = m_N;
     // Beregner energien med periodic boundary conditions
     // m_E = 0;
     // int jm = m_N;
     // for (int j = 1; j <= m_N; j++) {
-    //     m_E -= m_J*m_spin[j]*m_spin[jm];
+    //     m_E -= m_spin[j]*m_spin[jm];
     //     jm = j;
     // }
     // cout << m_E << endl;
-    m_E = -8;
+    m_spin[0] = 1;
+    m_spin[1] = -1;
+    m_spin[2] = -1;
+    m_spin[3] = 1;
+    m_M = 0;
+    m_E = 8;
 
     // Setter opp mulige Boltzmann-faktorer:
     m_BoltzmannFactor = new double[17];
-    m_BoltzmannFactor[0] = exp(-8);
-    m_BoltzmannFactor[4] = exp(-4);
+    m_BoltzmannFactor[0] = exp(8);
+    m_BoltzmannFactor[4] = exp(4);
     m_BoltzmannFactor[8] = exp(0);
-    m_BoltzmannFactor[12] = exp(4);
-    m_BoltzmannFactor[16] = exp(8);
+    m_BoltzmannFactor[12] = exp(-4);
+    m_BoltzmannFactor[16] = exp(-8);
 }
 
 int IsingModel::Periodic(int i, int add){
-    return (i + add) % m_L;
+    return (i + m_L + add) % m_L;
 }
 
 void IsingModel::Metropolis(){
@@ -75,37 +80,17 @@ void IsingModel::Metropolis(){
 }
 
 void IsingModel::MonteCarlo(int cycles) {
-    // Initialiserer forventningsverdier
-    m_Eavg = 0.0;
-    m_Mavg = 0.0;
-    double avg_M = 0.0;
-    double avg_E2 = 0.0;
-    double avg_M2 = 0.0;
+    string filename = "results.txt";
+    ofile.open(filename);
     for (int i = 0; i < cycles; i++) {
         Metropolis();
-        m_Eavg += m_E;
-        m_Mavg += abs(m_M);
-        avg_M += m_M;
-        avg_E2 += m_E*m_E;
-        avg_M2 += m_M*m_M;
+        ofile << setw(15) << setprecision(8) << m_E;
+        ofile << setw(15) << setprecision(8) << m_M << endl;
     }
-    m_Eavg /= cycles;
-    m_Mavg /= cycles;
-    avg_M /= cycles;
-    avg_E2 /= cycles;
-    avg_M2 /= cycles;
-    m_CV = 1/(m_temp*m_temp)*(avg_E2 - m_Eavg*m_Eavg);
-    m_chi = 1/m_temp*(avg_M2 - avg_M*avg_M);
+    delete[] m_BoltzmannFactor;
+    delete[] m_spin;
 }
 
 void IsingModel::Write_to_file(){
-    string filename = "results.txt";
-    ofile.open(filename);
-    ofile << setw(15) << setprecision(8) << m_temp;
-    ofile << setw(15) << setprecision(8) << m_Eavg;
-    ofile << setw(15) << setprecision(8) << m_Mavg;
-    ofile << setw(15) << setprecision(8) << m_CV;
-    ofile << setw(15) << setprecision(8) << m_chi << endl;
-    delete[] m_BoltzmannFactor;
-    delete[] m_spin;
+
 }
--- Project4/main.cpp
+++ Project4/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     IsingModel my_solver;
     my_solver.Initialize(2, 1.0);
-    my_solver.MonteCarlo(10000);
-    my_solver.Write_to_file();
+    my_solver.MonteCarlo(10);
+    // my_solver.Write_to_file();
     return 0;
 }
