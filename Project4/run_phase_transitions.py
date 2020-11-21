2810c67f5dff98654ecf17d4c62f5693c73a7733
pederlh <36080334+pederlh@users.noreply.github.com>
2020-11-22 00:17:28 +0100

    La til kompilasjonsflagg

--- Project4/run_phase_transitions.py
+++ Project4/run_phase_transitions.py
@@ -6,20 +6,21 @@ import sys
 # ------------- Compilation -------------
 all_cpp_codes = "./*.cpp"
 os.system("echo compiling...")
-os.system("c++ -o main.exe" + " " + all_cpp_codes)
+os.system("g++ -O3 -fopenmp -o main.exe" + " " + all_cpp_codes)
 # ---------------------------------------
 
 cycles = 1000
+threads = 12
 
-header_str = " Lattice size L  Temperature T      MC Cycles            <E>            <M>        Threads       Time (s)"
+header_str = " Lattice size L  Temperature T      MC Cycles          <E>/N          <M>/N        Threads       Time (s)"
 os.system(f'echo "{header_str}"  >> results.txt')
 
 for L in [40, 60, 80, 100]:
     for T in [2.0, 2.05, 2.1, 2.15, 2.2, 2.25, 2.3]:
         os.system("echo  ")
         os.system("echo executing for L = " + str(L) + ", T = " + str(T) + "...")
-        os.system("./main.exe " + str(L) + " " + str(T) + " " + str(cycles))    # Execute code
+        os.system("./main.exe " + str(L) + " " + str(T) + " " + str(cycles) + " " + str(threads))    # Execute code
 
-new_name = "phase_transitions.txt"
-os.rename("results.txt", new_name)
-os.system("mv " + new_name +  " results")           # Move data to results directory.
+# new_name = "phase_transitions.txt"
+# os.rename("results.txt", new_name)
+# os.system("mv " + new_name +  " results")           # Move data to results directory.
