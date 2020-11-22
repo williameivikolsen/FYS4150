57e7246e4fef2a5ecd8c2d75a4372017ce9f1231
Benedicte Nyheim <benedibn@student.matnat.uio.no>
2020-11-22 12:46:46 +0100

    abs av M ++

--- Project4/IsingModel.cpp
+++ Project4/IsingModel.cpp
@@ -2,16 +2,11 @@
 using namespace std;
 ofstream ofile;
 
-void IsingModel::Initialize(int L, double temp, int cycles, bool random_config, int seed_shift){
+void IsingModel::Initialize(int L, double temp, bool random_config){
     m_L = L;
     m_temp = temp;
-    m_cycles = cycles;
     m_N = m_L*m_L;
     m_spin = new int[m_N];
-
-    // random_device rand_nb;
-    mt19937 gen(clock()+100*seed_shift);
-    uniform_int_distribution<int> zero_one_int_dist(0, 1);
 
     if (random_config==false) {
         // Initialize lattice with all spins pointing the same direction (down)
@@ -24,9 +19,12 @@ void IsingModel::Initialize(int L, double temp, int cycles, bool random_config, 
     }
     else {
       // Initialize lattice with random spin configuration
+      random_device rand_nb;
+      mt19937_64 gen(rand_nb());
+      uniform_int_distribution<int> RandIntGen(0,1);
       for (int i = 0; i < m_L; i++) {
           for (int j = 0; j < m_L; j++) {
-              m_spin[i*m_L + j] = zero_one_int_dist(gen)*2-1;
+              m_spin[i*m_L + j] = RandIntGen(gen)*2-1;
               m_M += m_spin[i*m_L + j];
           }
       }
@@ -44,11 +42,11 @@ void IsingModel::Initialize(int L, double temp, int cycles, bool random_config, 
 
     // Setter opp mulige Boltzmann-faktorer:
     m_BoltzmannFactor = new double[17];
-    m_BoltzmannFactor[0] = exp(8/m_temp);
-    m_BoltzmannFactor[4] = exp(4/m_temp);
+    m_BoltzmannFactor[0] = exp(8);
+    m_BoltzmannFactor[4] = exp(4);
     m_BoltzmannFactor[8] = exp(0);
-    m_BoltzmannFactor[12] = exp(-4/m_temp);
-    m_BoltzmannFactor[16] = exp(-8/m_temp);
+    m_BoltzmannFactor[12] = exp(-4);
+    m_BoltzmannFactor[16] = exp(-8);
 }
 
 int IsingModel::Periodic(int i, int add){
@@ -60,16 +58,13 @@ void IsingModel::Metropolis(){
     int dE; int dM;         // Changes in energy and magnetization
     double w;               // Boltzmann factor
     double r;               // Random number
-
-    uniform_real_distribution<double> zero_one_real_dist(0, 1.0);
-    uniform_int_distribution<int> zero_L_dist(0, m_L-1);
-
+    srand(time(0));
     // Go through all elements and perform random spin flip each time
     for (int i = 0; i <=m_L; i++) {
         for (int j = 0; j < m_L; j++) {
             // Generate random numbers x and y in range [0, L-1]
-            x = zero_L_dist(gen);
-            y = zero_L_dist(gen);
+            x = rand()%m_L;
+            y = rand()%m_L;
             dE = 2*m_spin[x*m_L + y]*
             (m_spin[Periodic(x, 1)*m_L + y] +
             m_spin[Periodic(x, -1)*m_L + y] +
@@ -82,7 +77,7 @@ void IsingModel::Metropolis(){
             }
             else {
                 w = m_BoltzmannFactor[dE+8];
-                r = zero_one_real_dist(gen);
+                r = rand()*1./RAND_MAX;
                 if (r <= w) {
                     m_spin[x*m_L + y] *= -1;
                     m_E += dE;
@@ -93,45 +88,29 @@ void IsingModel::Metropolis(){
     }
 }
 
-void IsingModel::MonteCarlo() {
+void IsingModel::MonteCarlo(int cycles) {
     m_Eavg = 0.0;
     m_Mavg = 0.0;
-
-    for (int i = 0; i < m_cycles; i++) {
+    string filename = "results.txt";
+    ofile.open(filename, ios_base::app);
+    for (int i = 0; i < cycles; i++) {
         Metropolis();
-        if (i >= m_cycles*0.1-1){
+        if (i >= cycles*0.1-1){
           m_Eavg += m_E;
           m_Mavg += abs(m_M);
         }
     }
-    m_Eavg /= (0.9*m_cycles*m_N);
-    m_Mavg /= (0.9*m_cycles*m_N);
-    delete[] m_BoltzmannFactor;
-    delete[] m_spin;
-}
+    double norm = 1.0/(0.9*cycles*m_N);
+    m_Eavg *= norm;
+    m_Mavg *= norm;
+    m_M2avg *= norm;
+    m_E2avg *= norm;
 
-void IsingModel::WriteToFile(double time_used){
-    string filename = "results.txt";
-    ofile.open(filename, ios_base::app);
-    ofile << setw(15) << setprecision(8) << m_L;
-    ofile << setw(15) << setprecision(8) << m_temp;
-    ofile << setw(15) << setprecision(8) << m_cycles;
-    ofile << setw(15) << setprecision(8) << m_Eavg;
-    ofile << setw(15) << setprecision(8) << m_Mavg;
-    ofile << setw(15) << setprecision(8) << "1";                // Number threads
-    ofile << setw(15) << setprecision(8) << time_used << endl;
-    ofile.close();
-}
-
-void IsingModel::WriteToFileParallelized(double global_Eavg, double global_Mavg, int cycles, int threads, double time_used){
-    string filename = "results.txt";
-    ofile.open(filename, ios_base::app);
     ofile << setw(15) << setprecision(8) << m_L;
     ofile << setw(15) << setprecision(8) << m_temp;
     ofile << setw(15) << setprecision(8) << cycles;
-    ofile << setw(15) << setprecision(8) << global_Eavg;
-    ofile << setw(15) << setprecision(8) << global_Mavg;
-    ofile << setw(15) << setprecision(8) << threads;                // Number threads
-    ofile << setw(15) << setprecision(8) << time_used << endl;
-    ofile.close();
+    ofile << setw(15) << setprecision(8) << m_Eavg;
+    ofile << setw(15) << setprecision(8) << m_Mavg << endl;
+    delete[] m_BoltzmannFactor;
+    delete[] m_spin;
 }
