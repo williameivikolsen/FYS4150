709ee1923c4bfd4ed837ad20a1b217366ee825ff
pederlh <36080334+pederlh@users.noreply.github.com>
2020-11-23 22:26:40 +0100

    Har fått laget histogram

--- Project4/run_4e_probability_density.py
+++ Project4/run_4e_probability_density.py
@@ -19,14 +19,21 @@ def produce_new_data():
     T1 = 1.0                # Temp below Tc
     T2 = 2.4                # Temp above Tc
     cycles = 1e5
-    threads = 12    
-    cutoff_fraction = 0.0   # For this test: Don't cut away any results!
+    threads = 1             # This task can only be ran single-threaded (will give error)
+    cutoff_fraction = 0.1   # For this task: Keep cut-off at 10%
     bool_write_spins = 0
-    bool_write_energies = 0
+    bool_write_energies = 1 # Writes the total systems energy to text file "energies.txt"
     bool_random_config = 1
 
+    os.system("echo executing...")
+    # First make energy distribution for low temperature T = 1
     os.system("./main.exe " + str(L) + " " + str(T1) + " " + str(int(cycles)) + " " + str(bool_random_config) + " " + str(threads) + " " + str(cutoff_fraction) + " " + str(bool_write_spins) + " " + str(bool_write_energies))
+    # Move results to subfolder
+    os.system("mv 'energies.txt' './results/4e_probability_density_lowT.txt'")
+    # Then make new distribution for high temperature T = 2.4
     os.system("./main.exe " + str(L) + " " + str(T2) + " " + str(int(cycles)) + " " + str(bool_random_config) + " " + str(threads) + " " + str(cutoff_fraction) + " " + str(bool_write_spins) + " " + str(bool_write_energies))
+    # Move results to subfolder
+    os.system("mv 'energies.txt' './results/4e_probability_density_highT.txt'")
 
     os.system("rm results.txt")     # We are not interested in keeping the main results file for this task
 
@@ -34,8 +41,11 @@ def produce_new_data():
 """ -------------------- Load data --------------------  """
 
 # If not data doesn't already exist, run simulation
-path = "./results/4e_probability_density.txt"
-if not os.path.exists(path):
+
+path1 = "./results/4e_probability_density_lowT.txt"
+path2 = "./results/4e_probability_density_highT.txt"
+
+if not os.path.exists(path1) or not os.path.exists(path2):
     print("Simulation data not found in results subfolder. Running simulations...")
     produce_new_data()
 
@@ -55,29 +65,35 @@ plt.style.use('seaborn')
 seaborn.set(font_scale=1.3)
 os.chdir('./results')
 
-# data_aligned = np.loadtxt('4d_equilibrium_time_aligned.txt', skiprows=1)
-# T, cycles, E_aligned, acceptancerate_aligned = data_aligned[:, 1], data_aligned[:, 2], data_aligned[:, 3], data_aligned[:,9]
+E_lowT = np.loadtxt('4e_probability_density_lowT.txt')
+E_highT = np.loadtxt('4e_probability_density_highT.txt')
 
-# data_random = np.loadtxt('4d_equilibrium_time_random.txt', skiprows=1)
-# E_random, acceptancerate_random = data_random[:, 3], data_random[:,9]
+variance_lowT = np.var(E_lowT)
+variance_highT = np.var(E_highT)
+
+# Print variance of energies
+print(f"Variance of energies T=1.0: {variance_lowT}")
+print(f"Variance of energies T=2.4: {variance_highT}")   
 
 
-# T1 = 1.0    # Temp below Tc
-# T2 = 2.4    # Temp above Tc
+# Figure 1: Low T histogram
+plt.figure(1)
+plt.hist(E_lowT, bins=np.arange(min(E_lowT)-4, max(E_lowT)-4 + 8, 8), bottom=-4, label='T = 1.0 [J/$k_B$]')
+plt.xlabel(r'System energy $E$ [J]')
+plt.ylabel(r'Accurences')
+plt.legend()
+plt.tight_layout()
+plt.savefig("4e_histogram_lowT.pdf")
+os.system("mv 4e_histogram_lowT.pdf ../plots")
 
-# idx_dict = {i: np.where(T == i) for i in [T1, T2]}
-    
-# Figure 1: Energies for T = 1
-# plt.figure(1)
-# plt.plot(cycles[idx_dict[T1]], E_aligned[idx_dict[T1]], '-o', label='Aligned initialization')
-# plt.plot(cycles[idx_dict[T1]], E_random[idx_dict[T1]], '-o', label='Random initialization')
-# plt.xlabel("Monte Carlo cycles")
-# plt.ylabel(r'Average energy per spin $\langle E\rangle/L^2$ [J]')
-# plt.xscale('log')
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig("4d_equilibrium_time_lowT_energy.pdf")
-# os.system("mv 4d_equilibrium_time_lowT_energy.pdf ../plots")
+plt.figure(2)
+plt.hist(E_highT, bins=np.arange(min(E_highT)-4, max(E_highT)-4 + 8, 8), bottom=-4, label='T = 2.4 [J/$k_B$]')
+plt.xlabel(r'System energy $E$ [J]')
+plt.ylabel(r'Accurences')
+plt.legend()
+plt.tight_layout()
+plt.savefig("4e_histogram_highT.pdf")
+os.system("mv 4e_histogram_highT.pdf ../plots")
 
 # Show produced plots
-# plt.show()
+plt.show()
