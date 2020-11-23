cd12d16c83109b955d78d8cea62b859db8e0c26b
williameivikolsen <william@LAPTOP-BI6LPL6C.localdomain>
2020-11-23 09:16:50 +0100

    La til for-loopene som skulle være der igjen

--- Project4/plot_phase_transitions.py
+++ Project4/plot_phase_transitions.py
@@ -19,7 +19,7 @@ idx_dict = {i: np.where(L == i) for i in Lvals}
 
 for L in Lvals:
     idx = idx_dict[L]
-    plt.plot(T[idx], M[idx], '-o', label='L = %i' % L)
+    plt.plot(T[idx], M[idx], 'o', label='L = %i' % L)
 plt.xlabel(r'$T$')
 plt.ylabel(r'$\langle|M|\rangle/L^2$')
 plt.legend()
