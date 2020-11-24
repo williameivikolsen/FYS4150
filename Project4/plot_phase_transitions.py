67eec1a620efad1530895448e29c96e4ad643571
pederlh <36080334+pederlh@users.noreply.github.com>
2020-11-24 09:44:42 +0100

    Nye faseoverganger

--- Project4/plot_phase_transitions.py
+++ Project4/plot_phase_transitions.py
@@ -9,7 +9,7 @@ sns.set(font_scale=1.3)
 
 os.chdir('./results')
 data = np.loadtxt('phase_transitions.txt', skiprows=1)
-# data = np.loadtxt('phase_transitions_copy_nov22.txt', skiprows=1)
+# data = np.loadtxt('phase_transitions_copy_nov24.txt', skiprows=1)
 
 L = data[:, 0]
 T = data[:, 1]
@@ -24,9 +24,9 @@ idx_dict = {i: np.where(L == i) for i in Lvals}
 
 for L in Lvals:
     idx = idx_dict[L]
-    plt.plot(T[idx], M[idx], 'o', label='L = %i' % L)
+    plt.plot(T[idx], CV[idx], '-o', label='L = %i' % L)
 plt.xlabel(r'$T$ [kT/J]')
-plt.ylabel(r'$\langle |M| \rangle /N$')
+plt.ylabel(r'$\chi /N$')
 plt.legend()
 plt.tight_layout()
 plt.show()
@@ -35,5 +35,5 @@ plt.show()
 TC = {}
 for L in Lvals:
     idx = idx_dict[L]
-    TC[L] = T[np.argmax(CV[idx])]
+    TC[L] = T[np.argmax(chi[idx])]
 print(TC)
