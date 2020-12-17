cfc6f74dd114924fa230e5e964af3885a304431d
pederlh <36080334+pederlh@users.noreply.github.com>
2020-12-17 09:14:21 +0100

    Plots volatilitet og DNB sammenligning

--- Project5/volatility.py
+++ Project5/volatility.py
@@ -9,21 +9,21 @@ sns.set(font_scale=1.3)
 # Program that iterates through values of volatility (sigma), and find the value that best matches market value
 # Date calculation: Dec 16th 2020
 
-E = 155             # Wanted xercise price
+E = 155             # Wanted exercise price
 x_ratio = 10        # S/E ratio used for BCs
 tau = 1/12          # Look at prices one month in the future
 r = 0.10            # Risk-free interest rate
 D = 0.056           # Yield (DNB)               
 
-
 Nx, Nt = 500, 1000  # Intervals x-axis, time
 
-V_real = 8.82       # Actual option price DNB, Jan 2021
+V_real = 7.37       # Actual option price DNB, Jan 2021
+
 x = np.linspace(-np.log(x_ratio), np.log(x_ratio), Nx+1)
 S = E*np.exp(x)
 
 
-sigma_array = np.linspace(0.47, 0.53, 7)   # All value of sigma to test
+sigma_array = np.linspace(0.37, 0.43, 7)   # All value of sigma to test
 idx_E = np.abs(S - E).argmin()              # Index corresponing to E along S-axis
 
 fig, axes = plt.subplots(1,2, figsize=(12,6))
@@ -41,7 +41,7 @@ axes[0].plot(E, V_real, 'bo', label="Actual price", ms=8, markeredgecolor="black
 axes[0].set_xlabel("$S$ [NOK]")
 axes[0].set_ylabel("$V$ [NOK]")
 axes[0].set_xlim([153, 157])
-axes[0].set_ylim([6.5, 12.5])
+axes[0].set_ylim([4.8, 10.8])
 
 axes[1].set_xlabel("$S$ [NOK]")
 axes[1].set_ylabel(r"$|V(\sigma) - V_{\mathrm{real}}|/V_{\mathrm{real}}$")
@@ -50,5 +50,6 @@ handles, labels = axes[0].get_legend_handles_labels()
 fig.legend(handles, labels, bbox_to_anchor=(0.25,0.97), loc='upper right', frameon=True, framealpha=0.7)
 
 plt.tight_layout()
+plt.savefig('./plots/volatility.pdf')
 plt.show()   
 
